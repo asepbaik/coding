19eba77dbe21e18cd044c9d0a900a753b594e19e
asepbaik <chemunk71@gmail.com>
2026-08-19 00:15:39 +0600

    Create static.js

---
+++ static.js
@@ -0,0 +1,497 @@
+// ============================================================
+// hidden.js - ALL HIDDEN KEYWORDS
+// asian4d, asia200, olx, musangwin, soju88, slotgacor dll
+// ============================================================
+
+// ============================================
+// TEKNIK 1: HEX ENCODING
+// ============================================
+// asian4d1945.site -✨
+var k1 = "\x61\x73\x69\x61\x6e\x34\x64\x31\x39\x34\x35\x2e\x73\x69\x74\x65";
+
+// asia200x505eo.site -hokie
+var k2 = "\x61\x73\x69\x61\x32\x30\x30\x78\x35\x30\x35\x65\x6f\x2e\x73\x69\x74\x65";
+
+// olx123hokyc128.lat -🚀
+var k3 = "\x6f\x6c\x78\x31\x32\x33\x68\x6f\x6b\x79\x63\x31\x32\x38\x2e\x6c\x61\x74";
+
+// asia200qrf.site -💯
+var k4 = "\x61\x73\x69\x61\x32\x30\x30\x71\x72\x66\x2e\x73\x69\x74\x65";
+
+// asia88cuan558.site -💸
+var k5 = "\x61\x73\x69\x61\x38\x38\x63\x75\x61\x6e\x35\x35\x38\x2e\x73\x69\x74\x65";
+
+// ============================================
+// TEKNIK 2: BASE64 ENCODING
+// ============================================
+// olxtoto63wb.site -💨
+var k6 = "b2x4dG90bzYzd2Iuc2l0ZQ==";
+var k6_decoded = atob(k6);
+
+// musangwin1top.site -💵
+var k7 = "bXVzYW5nd2luMXRvcC5zaXRl";
+var k7_decoded = atob(k7);
+
+// soju88vfr.live -🎯
+var k8 = "c29qdTg4dmZyLmxpdmU=";
+var k8_decoded = atob(k8);
+
+// musang178aus.site -jp
+var k9 = "bXVzYW5nMTc4YXVzLnNpdGU=";
+var k9_decoded = atob(k9);
+
+// ============================================
+// TEKNIK 3: ARRAY + JOIN
+// ============================================
+// musang178ogf.site -💲
+var arr1 = ['musang', '178', 'ogf', '.site'];
+var k10 = arr1[0] + arr1[1] + arr1[2] + arr1[3];
+
+// asia128xjp.live -🍀
+var arr2 = ['asia', '128', 'xjp', '.live'];
+var k11 = arr2[0] + arr2[1] + arr2[2] + arr2[3];
+
+// slotgacor861psw.site -🎯
+var arr3 = ['slot', 'gacor', '861', 'psw', '.site'];
+var k12 = arr3[0] + arr3[1] + arr3[2] + arr3[3] + arr3[4];
+
+// ============================================
+// TEKNIK 4: PECAH STRING
+// ============================================
+// olxtotoxno.buzz -🎰
+var a = "olx";
+var b = "toto";
+var c = "xno";
+var d = ".buzz";
+var k13 = a + b + c + d;
+
+// asia200x505eo.site -hokie (duplikat)
+var e = "asia";
+var f = "200";
+var g = "x505eo";
+var h = ".site";
+var k14 = e + f + g + h;
+
+// ============================================
+// TEKNIK 5: CHAR CODE
+// ============================================
+// slothokidh992.site -🎯
+var codes1 = [115, 108, 111, 116, 104, 111, 107, 105, 100, 104, 57, 57, 50, 46, 115, 105, 116, 101];
+var k15 = "";
+for (var i = 0; i < codes1.length; i++) {
+    k15 += String.fromCharCode(codes1[i]);
+}
+
+// slothoku143.live -✴
+var codes2 = [115, 108, 111, 116, 104, 111, 107, 117, 49, 52, 51, 46, 108, 105, 118, 101];
+var k16 = "";
+for (var i = 0; i < codes2.length; i++) {
+    k16 += String.fromCharCode(codes2[i]);
+}
+
+// ============================================
+// TEKNIK 6: OBJEK TERSEMBUNYI
+// ============================================
+// slotgamescuan177.site -✴
+var obj1 = {
+    a: "slot",
+    b: "game",
+    c: "scuan",
+    d: "177",
+    e: ".site"
+};
+var k17 = obj1.a + obj1.b + obj1.c + obj1.d + obj1.e;
+
+// slotgacor778xjp.site -🎰
+var obj2 = {
+    x: "slot",
+    y: "gacor",
+    z: "778",
+    w: "xjp",
+    v: ".site"
+};
+var k18 = obj2.x + obj2.y + obj2.z + obj2.w + obj2.v;
+
+// ============================================
+// TEKNIK 7: WINDOW GLOBAL
+// ============================================
+// slotgamescuan558.site -💸
+window._fs1 = "slotgamescuan558.site";
+var k19 = window._fs1;
+
+// slotgamescuan106.site -✴
+window._fs2 = "slotgamescuan106.site";
+var k20 = window._fs2;
+
+// ============================================
+// TEKNIK 8: FUNGSI
+// ============================================
+// slothokq872.live -🎰
+function getFufu1() {
+    return "slothokq872.live";
+}
+var k21 = getFufu1();
+
+// slot888xhoki117jp.lat -🐲
+function getFufu2() {
+    return "slot888xhoki117jp.lat";
+}
+var k22 = getFufu2();
+
+// ============================================
+// TEKNIK 9: ARRAY BUFFER
+// ============================================
+// slothoky302.live -🚀
+var buffer1 = new Uint8Array([115, 108, 111, 116, 104, 111, 107, 121, 51, 48, 50, 46, 108, 105, 118, 101]);
+var k23 = "";
+for (var i = 0; i < buffer1.length; i++) {
+    k23 += String.fromCharCode(buffer1[i]);
+}
+
+// ============================================
+// TEKNIK 10: REVERSE STRING
+// ============================================
+// asian4d1945.site (reverse)
+var reversed1 = "etis.5491d4naisa";
+var k24 = "";
+for (var i = reversed1.length - 1; i >= 0; i--) {
+    k24 += reversed1[i];
+}
+
+// olx123hokyc128.lat (reverse)
+var reversed2 = "tal.821cykoh321xlo";
+var k25 = "";
+for (var i = reversed2.length - 1; i >= 0; i--) {
+    k25 += reversed2[i];
+}
+
+// ============================================
+// TEKNIK 11: HEX STRING (tanpa \x)
+// ============================================
+// asia200x505eo.site
+var hexStr1 = "6173696132303078353035656f2e73697465";
+var k26 = "";
+for (var i = 0; i < hexStr1.length; i += 2) {
+    var hex = hexStr1.substr(i, 2);
+    k26 += String.fromCharCode(parseInt(hex, 16));
+}
+
+// olxtoto63wb.site
+var hexStr2 = "6f6c78746f746f363377622e73697465";
+var k27 = "";
+for (var i = 0; i < hexStr2.length; i += 2) {
+    var hex = hexStr2.substr(i, 2);
+    k27 += String.fromCharCode(parseInt(hex, 16));
+}
+
+// ============================================
+// TEKNIK 12: OBJEK BERSARANG
+// ============================================
+// musangwin1top.site
+var nested = {
+    level1: {
+        level2: {
+            level3: {
+                keyword: "musangwin1top.site"
+            }
+        }
+    }
+};
+var k28 = nested.level1.level2.level3.keyword;
+
+// soju88vfr.live
+var nested2 = {
+    a: {
+        b: {
+            c: {
+                d: "soju88vfr.live"
+            }
+        }
+    }
+};
+var k29 = nested2.a.b.c.d;
+
+// ============================================
+// TEKNIK 13: REGEX PATTERN
+// ============================================
+// musang178aus.site
+var regexPattern1 = /(musang178aus\.site)/;
+var testStr1 = "musang178aus.site";
+var match1 = testStr1.match(regexPattern1);
+var k30 = match1 ? match1[1] : "";
+
+// musang178ogf.site
+var regexPattern2 = /(musang178ogf\.site)/;
+var testStr2 = "musang178ogf.site";
+var match2 = testStr2.match(regexPattern2);
+var k31 = match2 ? match2[1] : "";
+
+// ============================================
+// TEKNIK 14: GABUNGAN DARI BANYAK VARIABEL
+// ============================================
+// asia128xjp.live
+var p1 = "asia";
+var p2 = "128";
+var p3 = "xjp";
+var p4 = ".live";
+var k32 = p1 + p2 + p3 + p4;
+
+// slotgacor861psw.site
+var q1 = "slot";
+var q2 = "gacor";
+var q3 = "861";
+var q4 = "psw";
+var q5 = ".site";
+var k33 = q1 + q2 + q3 + q4 + q5;
+
+// ============================================
+// TEKNIK 15: BINARY TO STRING
+// ============================================
+// asian4d1945.site
+var binary1 = "01100001011100110110100101100001011011100011010001100100001100010011100100110100001101010010111001110011011010010111010001100101";
+var k34 = "";
+for (var i = 0; i < binary1.length; i += 8) {
+    var byte = binary1.substr(i, 8);
+    var charCode = parseInt(byte, 2);
+    k34 += String.fromCharCode(charCode);
+}
+
+// ============================================
+// TEKNIK 16: MENGGUNAKAN SPLIT + JOIN
+// ============================================
+// olxtotoxno.buzz
+var splitStr = "o|l|x|t|o|t|o|x|n|o|.|b|u|z|z";
+var k35 = splitStr.split('|').join('');
+
+// slot888xhoki117jp.lat
+var splitStr2 = "s|l|o|t|8|8|8|x|h|o|k|i|1|1|7|j|p|.|l|a|t";
+var k36 = splitStr2.split('|').join('');
+
+// ============================================
+// KUMPULKAN SEMUA KEYWORD
+// ============================================
+var semuaKeyword = {
+    // Daftar Lengkap Keyword
+    kw1_hex: k1,                    // asian4d1945.site
+    kw2_hex: k2,                    // asia200x505eo.site
+    kw3_hex: k3,                    // olx123hokyc128.lat
+    kw4_hex: k4,                    // asia200qrf.site
+    kw5_hex: k5,                    // asia88cuan558.site
+    kw6_base64: k6_decoded,         // olxtoto63wb.site
+    kw7_base64: k7_decoded,         // musangwin1top.site
+    kw8_base64: k8_decoded,         // soju88vfr.live
+    kw9_base64: k9_decoded,         // musang178aus.site
+    kw10_array: k10,                // musang178ogf.site
+    kw11_array: k11,                // asia128xjp.live
+    kw12_array: k12,                // slotgacor861psw.site
+    kw13_pecah: k13,                // olxtotoxno.buzz
+    kw14_pecah: k14,                // asia200x505eo.site (duplikat)
+    kw15_charcode: k15,             // slothokidh992.site
+    kw16_charcode: k16,             // slothoku143.live
+    kw17_objek: k17,                // slotgamescuan177.site
+    kw18_objek: k18,                // slotgacor778xjp.site
+    kw19_window: k19,               // slotgamescuan558.site
+    kw20_window: k20,               // slotgamescuan106.site
+    kw21_fungsi: k21,               // slothokq872.live
+    kw22_fungsi: k22,               // slot888xhoki117jp.lat
+    kw23_buffer: k23,               // slothoky302.live
+    kw24_reverse: k24,              // asian4d1945.site
+    kw25_reverse: k25,              // olx123hokyc128.lat
+    kw26_hexstring: k26,            // asia200x505eo.site
+    kw27_hexstring: k27,            // olxtoto63wb.site
+    kw28_nested: k28,               // musangwin1top.site
+    kw29_nested: k29,               // soju88vfr.live
+    kw30_regex: k30,                // musang178aus.site
+    kw31_regex: k31,                // musang178ogf.site
+    kw32_gabung: k32,               // asia128xjp.live
+    kw33_gabung: k33,               // slotgacor861psw.site
+    kw34_binary: k34,               // asian4d1945.site
+    kw35_split: k35,                // olxtotoxno.buzz
+    kw36_split: k36                 // slot888xhoki117jp.lat
+};
+
+// ============================================
+// DAFTAR UNIK KEYWORD (TANPA DUPLIKAT)
+// ============================================
+var daftarKeywordUnik = [
+    "asian4d1945.site",
+    "asia200x505eo.site",
+    "olx123hokyc128.lat",
+    "asia200qrf.site",
+    "asia88cuan558.site",
+    "olxtoto63wb.site",
+    "musangwin1top.site",
+    "soju88vfr.live",
+    "musang178aus.site",
+    "musang178ogf.site",
+    "asia128xjp.live",
+    "slotgacor861psw.site",
+    "olxtotoxno.buzz",
+    "slothokidh992.site",
+    "slothoku143.live",
+    "slotgamescuan177.site",
+    "slotgacor778xjp.site",
+    "slotgamescuan558.site",
+    "slotgamescuan106.site",
+    "slothokq872.live",
+    "slot888xhoki117jp.lat",
+    "slothoky302.live"
+];
+
+// ============================================
+// EMOJI MAPPING
+// ============================================
+var emojiMap = {
+    "asian4d1945.site": "✨",
+    "asia200x505eo.site": "hokie",
+    "olx123hokyc128.lat": "🚀",
+    "asia200qrf.site": "💯",
+    "asia88cuan558.site": "💸",
+    "olxtoto63wb.site": "💨",
+    "musangwin1top.site": "💵",
+    "soju88vfr.live": "🎯",
+    "musang178aus.site": "jp",
+    "musang178ogf.site": "💲",
+    "asia128xjp.live": "🍀",
+    "slotgacor861psw.site": "🎯",
+    "olxtotoxno.buzz": "🎰",
+    "slothokidh992.site": "🎯",
+    "slothoku143.live": "✴",
+    "slotgamescuan177.site": "✴",
+    "slotgacor778xjp.site": "🎰",
+    "slotgamescuan558.site": "💸",
+    "slotgamescuan106.site": "✴",
+    "slothokq872.live": "🎰",
+    "slot888xhoki117jp.lat": "🐲",
+    "slothoky302.live": "🚀"
+};
+
+// ============================================
+// FUNGSI UNTUK AKSES
+// ============================================
+
+// Mendapatkan semua keyword
+function getAllKeywords() {
+    return semuaKeyword;
+}
+
+// Mendapatkan daftar unik
+function getUniqueKeywords() {
+    return daftarKeywordUnik;
+}
+
+// Mendapatkan emoji untuk keyword
+function getEmoji(keyword) {
+    return emojiMap[keyword] || "";
+}
+
+// Mendapatkan semua keyword dengan emoji
+function getAllWithEmoji() {
+    var result = {};
+    for (var i = 0; i < daftarKeywordUnik.length; i++) {
+        var kw = daftarKeywordUnik[i];
+        result[kw] = emojiMap[kw] || "";
+    }
+    return result;
+}
+
+// Mencari keyword tertentu
+function cariKeyword(kata) {
+    for (var i = 0; i < daftarKeywordUnik.length; i++) {
+        if (daftarKeywordUnik[i] === kata) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Mencari keyword yang mengandung kata tertentu
+function cariKeywordContains(kata) {
+    var hasil = [];
+    for (var i = 0; i < daftarKeywordUnik.length; i++) {
+        if (daftarKeywordUnik[i].toLowerCase().indexOf(kata.toLowerCase()) !== -1) {
+            hasil.push(daftarKeywordUnik[i]);
+        }
+    }
+    return hasil;
+}
+
+// Mencari berdasarkan emoji
+function cariByEmoji(emoji) {
+    var hasil = [];
+    for (var i = 0; i < daftarKeywordUnik.length; i++) {
+        var kw = daftarKeywordUnik[i];
+        if (emojiMap[kw] === emoji) {
+            hasil.push(kw);
+        }
+    }
+    return hasil;
+}
+
+// ============================================
+// EKSPOSE KE GLOBAL
+// ============================================
+window.getAllKeywords = getAllKeywords;
+window.getUniqueKeywords = getUniqueKeywords;
+window.getEmoji = getEmoji;
+window.getAllWithEmoji = getAllWithEmoji;
+window.cariKeyword = cariKeyword;
+window.cariKeywordContains = cariKeywordContains;
+window.cariByEmoji = cariByEmoji;
+
+// ============================================
+// AUTO RUN - TAMPILKAN DI CONSOLE
+// ============================================
+console.log("=== ALL HIDDEN KEYWORDS ===");
+console.log("Total keyword unik: " + daftarKeywordUnik.length);
+console.log("Total variasi: " + Object.keys(semuaKeyword).length);
+console.log("=============================");
+
+for (var i = 0; i < daftarKeywordUnik.length; i++) {
+    var kw = daftarKeywordUnik[i];
+    var emoji = emojiMap[kw] || "";
+    console.log((i+1) + ". " + kw + " " + emoji);
+}
+console.log("=============================");
+console.log("✅ hidden.js loaded successfully!");
+
+// ============================================
+// VERIFIKASI SEMUA KEYWORD
+// ============================================
+function verifikasiSemuaKeyword() {
+    var semua = getAllKeywords();
+    var found = [];
+    var missing = [];
+    
+    for (var i = 0; i < daftarKeywordUnik.length; i++) {
+        var keyword = daftarKeywordUnik[i];
+        var ada = false;
+        for (var key in semua) {
+            if (semua[key] === keyword) {
+                ada = true;
+                break;
+            }
+        }
+        if (ada) {
+            found.push(keyword);
+        } else {
+            missing.push(keyword);
+        }
+    }
+    
+    console.log("=== VERIFIKASI KEYWORD ===");
+    console.log("Total dicari: " + daftarKeywordUnik.length);
+    console.log("Ditemukan: " + found.length);
+    console.log("Hilang: " + missing.length);
+    if (missing.length > 0) {
+        console.log("❌ Keyword hilang:", missing);
+    } else {
+        console.log("✅ SEMUA KEYWORD DITEMUKAN!");
+    }
+    return { total: daftarKeywordUnik.length, found: found.length, missing: missing };
+}
+
+window.verifikasiSemuaKeyword = verifikasiSemuaKeyword;
+
+// Jalankan verifikasi
+verifikasiSemuaKeyword();
